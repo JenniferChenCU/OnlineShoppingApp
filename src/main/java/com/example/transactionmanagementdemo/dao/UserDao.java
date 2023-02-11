948956bc1053ea0ff19ac2fa0651c9d6332baff8
JenniferChenCU <91161902+JenniferChenCU@users.noreply.github.com>
2023-02-10 20:26:28 -0500

    first committ

---
+++ src/main/java/com/example/transactionmanagementdemo/dao/UserDao.java
@@ -0,0 +1,4 @@
+package com.example.transactionmanagementdemo.dao;
+
+public class UserDao {
+}
---
+++ src/main/java/com/example/transactionmanagementdemo/domain/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.transactionmanagementdemo.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="USER")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "isSeller")
+    private boolean isSeller;
+}
